f349424bcb26288b58932b3d13a330b73309c406
Nathan Anderson <nathan.anderson.1024@gmail.com>
2022-09-16 11:37:09 -0700

    added method for saving playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this)
     this.updatePlaylistName = this.updatePlaylistName.bind(this)
+    this.savePlaylist = this.savePlaylist.bind(this)
     this.state = {
       searchResults: [
         {
@@ -79,6 +80,9 @@ class App extends React.Component {
       playlistName: name
     })
   }
+  savePlaylist() {
+    const trackURIs = [this.state.playlistTracks]
+  }
   render() {
     return (
       <div>
@@ -88,7 +92,8 @@ class App extends React.Component {
           <div className='App-playlist'>
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}
-                      onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} />
+                      onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}
+                      onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
